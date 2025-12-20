59037473236159e2ca248273e409667eab9ff45e
Prashant Kumar <90041608+Prashantkr81@users.noreply.github.com>
2025-12-20 15:53:55 +0530

    Alternate and more readable approach

--- InsertIntervals.java
+++ InsertIntervals.java
@@ -25,3 +25,34 @@ class Solution {
         return result.toArray(new int[result.size()][]);
     }
 }
+
+
+---------------------------------------------------------
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> result= new ArrayList<>(intervals.length+1);
+
+        int n= intervals.length, i=0;
+
+        //add intervals to result before new Interval approaches
+        while(i<n && intervals[i][1] < newInterval[0]){
+            result.add(intervals[i]);
+            i++;
+        }
+        // merge overlapping interval
+        while(i<n && intervals[i][0] <= newInterval[1]){
+            newInterval[0]= Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1]= Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.add(newInterval);
+
+        // add rest of intervals to result
+        while(i<n){
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
