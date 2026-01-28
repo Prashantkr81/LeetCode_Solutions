76afdbf84cf6c6c04ccdf8ac2eda816e34e72b02
Prashant Kumar <90041608+Prashantkr81@users.noreply.github.com>
2026-01-28 18:36:52 +0530

    Implement sorted merge of two linked lists

---
+++ mergeLinkedList.java
@@ -0,0 +1,32 @@
+class Solution {
+    Node sortedMerge(Node head1, Node head2) {
+        // code here
+        Node newHead= new Node(0);
+        Node temp= newHead;
+        
+        while(head1!=null && head2!=null){
+            if(head1.data < head2.data){
+                temp.next= head1;
+                temp = head1;
+                head1= head1.next;
+            }
+            else{
+                temp.next= head2;
+                temp = head2;
+                head2= head2.next;
+            }
+        }
+        while(head1!=null){
+            temp.next = head1;
+            temp=temp.next;
+            head1= head1.next;
+        }
+        while(head2!=null){
+            temp.next = head2;
+            temp=temp.next;
+            head2= head2.next;
+        }
+        
+        return newHead.next;
+    }
+}
