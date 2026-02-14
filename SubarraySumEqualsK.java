d177177a63e2c26a882d91e79c69f1b7d45340df
Prashant Kumar <90041608+Prashantkr81@users.noreply.github.com>
2026-02-14 21:46:36 +0530

    Implement subarray sum equals k solution

---
+++ SubarraySumEqualsK.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int currSum=0, start=0, end=-1;
+
+        int ans=0;
+
+        for(int i=0; i<nums.length; i++){
+            currSum+= nums[i];
+
+            if(currSum==k){
+                ans++;
+            }
+
+            if(map.containsKey(currSum-k)){
+                ans+= map.get(currSum-k);
+            }
+            map.put(currSum, map.getOrDefault(currSum, 0)+1);
+        }
+        return ans;
+    }
+}
