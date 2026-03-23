05389b45556473c6a561b97d84ea067f947c616d
Prashant Kumar <90041608+Prashantkr81@users.noreply.github.com>
2026-03-23 20:05:31 +0530

    Implement House Robber II solution using dynamic programming

---
+++ House_Robber_II.java
@@ -0,0 +1,27 @@
+class Solution {
+    int dp[];
+    int solve(int nums[], int i, int n){
+        if(i >= n) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        int take= nums[i] + solve(nums, i+2, n);
+        int skip= solve(nums, i+1, n);
+
+        return dp[i]= Math.max(take, skip);
+    }
+    public int rob(int[] nums) {
+        int n= nums.length;
+        if(n==1) return nums[0];
+
+        dp= new int[n];
+
+        Arrays.fill(dp, -1);
+        int s1= solve(nums, 0, n-1);  //as its a circular aaray...if H0 is selected then Hn (last) cant be selected
+
+        Arrays.fill(dp, -1);
+        int s2= solve(nums, 1, n);  // same if last is selected then first cant be selected
+
+        return Math.max( s1, s2);
+    }
+}
