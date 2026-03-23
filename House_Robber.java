62c163b9cddb111330256fb2b7775fc908256e97
Prashant Kumar <90041608+Prashantkr81@users.noreply.github.com>
2026-03-23 20:02:57 +0530

    Implement House Robber solution using dynamic programming

---
+++ House_Robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    int dp[];
+    int solve(int nums[], int i){
+        if(i>=nums.length) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        int take= nums[i] + solve(nums, i+2);
+        int skip= solve(nums, i+1);
+
+        return dp[i]= Math.max(take, skip);
+    }
+    public int rob(int[] nums) {
+        dp= new int[nums.length];
+        Arrays.fill(dp, -1);
+
+        return solve(nums, 0);
+    }
+}
